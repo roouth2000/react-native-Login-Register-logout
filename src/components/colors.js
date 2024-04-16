55e831b419a8d4457c687e03812b1e9bffbe9371
roouth2000 <132045226+roouth2000@users.noreply.github.com>
2024-04-16 10:09:09 +0530

    Delete src directory

--- src/components/colors.js
+++
@@ -1,3 +0,0 @@
-// colors.js
-export const splashback = '#4169e1';
-export const SECONDARY_COLOR = '#ff6347';
--- src/theme/theme.js
+++
@@ -1,7 +0,0 @@
-export const COLORS = {
-    primary: "#6360DC",
-    dark: "#161719",
-    white: "#fff",
-    body: "#405792",
-    bg: "#ECECEC",
-  };
